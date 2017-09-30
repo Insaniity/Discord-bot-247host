e5a72d52b49889e173298b3611ae36cf392180d1
Insaniity <iiyolo@icloud.com>
2017-09-30 13:38:43 +0100

    global
    7

---
+++ cmds/global.js
@@ -0,0 +1,17 @@
+module.exports.run = (bot, message, args) => {
+    var cmdTxt = message.content.split(" ")[0].substring(1).toLowerCase();
+    var suffix = message.content.substring(cmdTxt.length + 2);
+    
+    bot.guilds.forEach(g => {
+
+      if (message.author.id !== "190174684371746818") return; {
+		g.defaultChannel.send(suffix)
+
+      }
+
+    })
+}
+
+module.exports.help = {
+	name: "g"
+}
